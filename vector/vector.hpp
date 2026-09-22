638efac305d82ada7a70a1b295c86937da91dc4d
Aarav Khemka <aarkhe451@fusdk12.net>
2026-09-22 08:23:23 -0700

    Refactor vector class and add push_back method
    Refactored constructor and destructor to use default implementations. Added push_back method with memory allocation logic.

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <memory>
+#include <utility>
 
 namespace reimplimentation {
     template <typename datatype>
     class vector {
         public:
-            vector() {}
-            ~vector() {}
+            vector() = default;
+            ~vector() = default;
+        
+            void push_back(datatype data) {
+                if (size == capacity) {
+                    size_t new_capacity = (capacity == 0) ? 1 : capacity * 2;
+                    size_t total_bytes = new_capacity * size_of(datatype);
+                    datatype* new_memory = alloc.allocate(new_capacity); // allocate new memory buffer
+
+                    for (size_t i = 0; i < size; i++) {
+                        //std::allocator_traits<std::allocator<datatype>>
+                    }
+                }
+            }
         private:
             unsigned int size = 0;
             unsigned int capacity = 0;
+            std::allocator<datatype> alloc;
+            datatype* memory = nullptr;
     };
 }
 
