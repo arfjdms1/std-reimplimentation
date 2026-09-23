38973e4d1110ec4f41f14df0e9345eb37d955669
aarkhe451 <aarkhe451@fusdk12.net>
2026-09-23 08:23:05 -0700

    Implement initial vector push_back allocation and growth

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -14,23 +14,10 @@ namespace reimplimentation {
             }
 
             ~vector() = default;
-        
+
             void push_back(datatype data) {
                 if (size == capacity) {
-                    size_t new_capacity = (capacity == 0) ? 1 : capacity * 2;
-                    datatype* new_memory = alloc.allocate(new_capacity); // allocate new memory buffer
-
-                    for (size_t i = 0; i < size; i++) { // copy data to new allocated array
-                        std::allocator_traits<std::allocator<datatype>>::construct(alloc, new_memory + i, memory[i]);
-                    }
-
-                    for (size_t i = 0; i < size; i++) { // dealoc objects of old array
-                        std::allocator_traits<std::allocator<datatype>>::destroy(alloc, memory + i);
-                    }
-
-                    alloc.deallocate(memory, capacity);
-                    memory = new_memory;
-                    capacity = new_capacity;
+                    reserve(size + 1);
                 }
 
                 std::allocator_traits<std::allocator<datatype>>::construct(alloc, memory + size, data); // copy new data
@@ -41,5 +28,28 @@ namespace reimplimentation {
             unsigned int capacity;
             std::allocator<datatype> alloc;
             datatype* memory;
+
+            void reserve(int requestedCapacity) {
+                size_t new_capacity = capacity;
+                while (new_capacity < requestedCapacity) {
+                    new_capacity = (new_capacity == 0) ? 1 : new_capacity * 2;
+                }
+
+                datatype* new_memory = alloc.allocate(new_capacity); // allocate new memory buffer
+
+                for (size_t i = 0; i < size; i++) { // copy data to new allocated array
+                    std::allocator_traits<std::allocator<datatype>>::construct(alloc, new_memory + i, memory[i]);
+                }
+
+                for (size_t i = 0; i < size; i++) { // dealoc objects of old array
+                    std::allocator_traits<std::allocator<datatype>>::destroy(alloc, memory + i);
+                }
+
+                if (memory != nullptr) {
+                    alloc.deallocate(memory, capacity);
+                }
+                memory = new_memory;
+                capacity = new_capacity;
+            }
     };
 }
