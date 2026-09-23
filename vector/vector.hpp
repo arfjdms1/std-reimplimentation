827ed5658986d4b145df36c153b2ce26e63690ff
Aarav Khemka <aarkhe451@fusdk12.net>
2026-09-23 19:30:38 +0000

    Refactor vector class for memory management and safety

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <iostream>
+#include <cstddef>
 #include <memory>
 #include <utility>
 
@@ -13,37 +13,62 @@ namespace reimplimentation {
                 memory = nullptr;
             }
 
-            ~vector() = default;
+            ~vector() {
+                if (memory != nullptr) {
+                    for (std::size_t i = 0; i < size; i++) {
+                        std::allocator_traits<std::allocator<datatype>>::destroy(alloc, memory + i);
+                    }
+                    alloc.deallocate(memory, capacity);
+                }
+            }
 
             void push_back(datatype data) {
-                if (size == capacity) {
-                    reserve(size + 1);
-                }
-
-                std::allocator_traits<std::allocator<datatype>>::construct(alloc, memory + size, data); // copy new data
+                reserve(size + 1);
+                std::allocator_traits<std::allocator<datatype>>::construct(alloc, memory + size, std::move(data)); // safely (AKA Fuck it We BALL) move data to new vector
                 size++;
             }
+
+            int length() {
+                return size;
+            }
+
         private:
-            unsigned int size;
-            unsigned int capacity;
+            std::size_t size;
+            std::size_t capacity;
             std::allocator<datatype> alloc;
             datatype* memory;
 
-            void reserve(int requestedCapacity) {
-                size_t new_capacity = capacity;
+            void reserve(std::size_t requestedCapacity) {
+                if (capacity >= requestedCapacity) { // Dont do unneeded work;
+                    return;
+                }
+
+                std::size_t new_capacity = capacity;
                 while (new_capacity < requestedCapacity) {
                     new_capacity = (new_capacity == 0) ? 1 : new_capacity * 2;
                 }
 
                 datatype* new_memory = alloc.allocate(new_capacity); // allocate new memory buffer
 
-                for (size_t i = 0; i < size; i++) { // copy data to new allocated array
-                    std::allocator_traits<std::allocator<datatype>>::construct(alloc, new_memory + i, memory[i]);
+                std::size_t constructed = 0;
+
+                try {
+                    for (std::size_t i = 0; i < size; i++) { // safely move data to new allocated array
+                        std::allocator_traits<std::allocator<datatype>>::construct(alloc, new_memory + i, std::move_if_noexcept(memory[i]));
+                        constructed++;
+                    }
+                } catch (...) { // Catch Exeptions
+                    for (std::size_t i = 0; i < constructed; i++) {
+                        std::allocator_traits<std::allocator<datatype>>::destroy(alloc, new_memory + i);
+                    }
+                    alloc.deallocate(new_memory, new_capacity);
+                    throw;
                 }
 
-                for (size_t i = 0; i < size; i++) { // dealoc objects of old array
+                for (std::size_t i = 0; i < size; i++) { // dealoc objects of old array
                     std::allocator_traits<std::allocator<datatype>>::destroy(alloc, memory + i);
                 }
+
 
                 if (memory != nullptr) {
                     alloc.deallocate(memory, capacity);
