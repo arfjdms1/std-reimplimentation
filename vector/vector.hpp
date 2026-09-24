63979912c4fd814b102c79738fb20d7f6c0187fb
Aarav <aarkhe451@fusdk12.net>
2026-09-23 20:57:04 -0700

    Add exception-safe copy assignment for vector

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -22,13 +22,57 @@ namespace reimplimentation {
                 }
             }
 
+            vector& operator=(const vector& other) {
+                if (other.size == 0) {
+                    if (memory != nullptr) {
+                        for (std::size_t i = 0; i < size; i++) {
+                            std::allocator_traits<std::allocator<datatype>>::destroy(alloc, memory + i);
+                        }
+                        alloc.deallocate(memory, capacity);
+                    }
+
+                    size = 0;
+                    capacity = 0;
+                    memory = nullptr;
+                } else if (this != &other) {
+                    datatype* new_memory = alloc.allocate(other.size);
+
+                    std::size_t constructed = 0;
+                    try {
+                        for (std::size_t i = 0; i < other.size; i++) {
+                            std::allocator_traits<std::allocator<datatype>>::construct(alloc, new_memory + i, other.memory[i]);
+                            constructed++;
+                        }
+                    } catch (...) {
+                        for (std::size_t i = 0; i < constructed; i++) {
+                            std::allocator_traits<std::allocator<datatype>>::destroy(alloc, new_memory + i);
+                        }
+                        alloc.deallocate(new_memory, other.size);
+                        throw;
+                    }
+
+                    for (std::size_t i = 0; i < size; i++) { // dealoc objects of old array
+                        std::allocator_traits<std::allocator<datatype>>::destroy(alloc, memory + i);
+                    }
+
+                    if (memory != nullptr) {
+                        alloc.deallocate(memory, capacity);
+                    }
+                    memory = new_memory;
+                    capacity = other.size;
+                    size = other.size;
+                }
+
+                return *this;
+            }
+
             void push_back(datatype data) {
                 reserve(size + 1);
                 std::allocator_traits<std::allocator<datatype>>::construct(alloc, memory + size, std::move(data)); // safely (AKA Fuck it We BALL) move data to new vector
                 size++;
             }
 
-            int length() {
+            std::size_t length() const {
                 return size;
             }
 
@@ -57,7 +101,7 @@ namespace reimplimentation {
                         std::allocator_traits<std::allocator<datatype>>::construct(alloc, new_memory + i, std::move_if_noexcept(memory[i]));
                         constructed++;
                     }
-                } catch (...) { // Catch Exeptions
+                } catch (...) { // Catch Exceptions
                     for (std::size_t i = 0; i < constructed; i++) {
                         std::allocator_traits<std::allocator<datatype>>::destroy(alloc, new_memory + i);
                     }
